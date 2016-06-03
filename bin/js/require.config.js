2969a3c81c360fca587deacda2bd71cdb1beeb0c
stefanbc <stefcosma@gmail.com>
2016-06-03 21:29:47 +0300

    First layout draft and added travis file

--- bin/js/require.config.js
+++ bin/js/require.config.js
@@ -1,8 +1,8 @@
 requirejs.config({
     baseUrl: 'bin/js',
     paths: {
-        jQuery      : 'http://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min',
-        humane      : 'http://cdnjs.cloudflare.com/ajax/libs/humane-js/3.2.2/humane.min',
+        jQuery      : '//ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min',
+        humane      : '//cdnjs.cloudflare.com/ajax/libs/humane-js/3.2.2/humane.min',
         app         : 'app.min'
     },
     shim: {
