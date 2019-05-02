eff0776aa2c3cd233476928e91036ca86a4e2686
stefanbc <stefcosma@gmail.com>
2019-05-03 00:11:24 +0300

    Clean-up for version 1.0

---
+++ assets/views/pug.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    locals: {
+        author_name : "Stefan Cosma",
+        author_url : "http://stefancosma.xyz",
+        project_name : "arca",
+        project_oneliner : "a simple aspect ratio calculator",
+        project_description : "Find the missing value for an aspect ratio!",
+        project_url : "https://github.com/stefanbc/arca",
+        project_version : "1.0.0",
+        app_url : "http://stefanbc.github.io/arca",
+        app_logo : "./assets/images/logo.png"
+    }
+};
